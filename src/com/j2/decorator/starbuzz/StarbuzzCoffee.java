e250538a96c68c868af8f344641464dc0001af26
chj8143 <chj8143@naver.com>
2015-10-10 20:33:45 +0900

    first

--- src/com/j2/decorator/starbuzz/StarbuzzCoffee.java
+++ src/com/j2/decorator/starbuzz/StarbuzzCoffee.java
@@ -1,11 +1,13 @@
 package com.j2.decorator.starbuzz;
 
+
 public class StarbuzzCoffee {
   public static void main(String args[]) {
     
     //Caramel Macchiato
     System.out.println("Caramel Macchiato:");
     Beverage beverage = new Espresso();
+    
      beverage = new BanillaSyrup(beverage);
      beverage = new SteamedMilk(beverage);
      beverage = new CaramelSource(beverage);
